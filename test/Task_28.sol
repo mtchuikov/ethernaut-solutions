9c91275d15f15833a46ddcc3a19d4f6e454dfa2f
misha <misha@yandex.ru>
2023-09-25 15:02:40 +0300

    final commit 100%

--- test/Task_28.sol
+++ test/Task_28.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-;
-
 import {Test, console} from "forge-std/Test.sol";
 
 import {SimpleTrick, GatekeeperThree} from "../src/task_28/GatekeeperThree.sol";
